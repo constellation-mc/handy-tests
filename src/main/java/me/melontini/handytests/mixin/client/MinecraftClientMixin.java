11f1c88ab06d59b6c521612dc526d3b03101a5bd
melontini <104443436+melontini@users.noreply.github.com>
2024-06-01 14:51:43 +0700

    Append version to the world directory.

--- src/main/java/me/melontini/handytests/mixin/client/MinecraftClientMixin.java
+++ src/main/java/me/melontini/handytests/mixin/client/MinecraftClientMixin.java
@@ -1,6 +1,8 @@
 package me.melontini.handytests.mixin.client;
 
 import me.melontini.handytests.util.Utils;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.SharedConstants;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.registry.RegistryKeys;
@@ -13,12 +15,19 @@ import net.minecraft.world.gen.GeneratorOptions;
 import net.minecraft.world.gen.WorldPresets;
 import net.minecraft.world.level.LevelInfo;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.regex.Pattern;
+
 @Mixin(value = MinecraftClient.class, priority = 1200)
 public class MinecraftClientMixin {
+
+    @Unique
+    private static final Pattern RESERVED_WINDOWS_NAMES = Pattern.compile(".*\\.|(?:COM|CLOCK\\$|CON|PRN|AUX|NUL|COM[1-9]|LPT[1-9])(?:\\..*)?", 2);
+
     @Inject(method = "method_29338", at = @At("TAIL"), require = 0)
     private void handy$init(CallbackInfo ci) {
         if (!Utils.ENABLED) return;
@@ -26,15 +35,20 @@ public class MinecraftClientMixin {
         MinecraftClient.getInstance().send(() -> {
             try {
                 MinecraftClient client = MinecraftClient.getInstance();
+                String levelName = "handy_test_" + FabricLoader.getInstance().getModContainer("minecraft").orElseThrow().getMetadata().getVersion().getFriendlyString();
 
-                if (!client.getLevelStorage().levelExists("handy_tests_world")) {
-                    client.createIntegratedServerLoader().createAndStart("handy_tests_world",
-                            new LevelInfo("handy_tests_world", GameMode.CREATIVE, false, Difficulty.PEACEFUL, true,
+                for (char c : SharedConstants.INVALID_CHARS_LEVEL_NAME) levelName = levelName.replace(c, '_');
+                levelName = levelName.replaceAll("[./\"]", "_");
+                if (RESERVED_WINDOWS_NAMES.matcher(levelName).matches()) levelName = "_" + levelName + "_";
+
+                if (!client.getLevelStorage().levelExists(levelName)) {
+                    client.createIntegratedServerLoader().createAndStart(levelName,
+                            new LevelInfo(levelName, GameMode.CREATIVE, false, Difficulty.PEACEFUL, true,
                                     new GameRules(), DataConfiguration.SAFE_MODE),
                             new GeneratorOptions(0, true, false),
                             registryManager -> registryManager.get(RegistryKeys.WORLD_PRESET).entryOf(WorldPresets.FLAT).value().createDimensionsRegistryHolder());
                 } else {
-                    client.createIntegratedServerLoader().start(new TitleScreen(), "handy_tests_world");
+                    client.createIntegratedServerLoader().start(new TitleScreen(), levelName);
                 }
             } catch (Throwable t) {
                 CrashReport report = CrashReport.create(t, "Setting tests world");
